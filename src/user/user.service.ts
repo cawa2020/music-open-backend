20a2b82c1e988d5603216b5e91dc523b27968b76
cawa <82109250+fast-typing@users.noreply.github.com>
2024-04-26 19:17:35 +0000

    swagger

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,8 +15,23 @@ export class UserService {
     return this.prisma.user.create({ data: body })
   }
 
-  async addSong(stringifySong: string, id: number) {
+  async addSong(id: number, strSong: string) {
     const favoriteSongs = (await this.prisma.user.findUnique({ where: { id: id } })).favoriteSongs
-    return this.prisma.user.update({ where: { id: id }, data: { favoriteSongs: [stringifySong, ...favoriteSongs] } })
+    return this.prisma.user.update({ where: { id: id }, data: { favoriteSongs: [strSong, ...favoriteSongs] } })
+  }
+
+  async addAlbum(id: number, strSong: string) {
+    const favoriteAlbums = (await this.prisma.user.findUnique({ where: { id: id } })).favoriteAlbums
+    return this.prisma.user.update({ where: { id: id }, data: { favoriteSongs: [strSong, ...favoriteAlbums] } })
+  }
+
+  async addPlaylist(id: number, strPlaylist: string) {
+    const favoritePlaylists = (await this.prisma.user.findUnique({ where: { id: id } })).favoritePlaylists
+    return this.prisma.user.update({ where: { id: id }, data: { favoriteSongs: [strPlaylist, ...favoritePlaylists] } })
+  }
+
+  async addArtist(id: number, strArtist: string) {
+    const favoriteArtists = (await this.prisma.user.findUnique({ where: { id: id } })).favoriteArtists
+    return this.prisma.user.update({ where: { id: id }, data: { favoriteSongs: [strArtist, ...favoriteArtists] } })
   }
 }
